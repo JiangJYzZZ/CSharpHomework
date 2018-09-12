417f428f66c4a0e294be9d8ebb00d36f48bc0d17
JiangJYzZZ <43143097+JiangJYzZZ@users.noreply.github.com>
2018-09-12 18:52:17 +0800

    Add files via upload

---
+++ firstWeekHomework/homework1_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace homework1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int n, m;
+            Console.Write("Plesase input a number:");
+            s = Console.ReadLine();
+            n = Int32.Parse(s);
+            Console.Write("Plesase input another number:");
+            s = Console.ReadLine();
+            m = Int32.Parse(s);
+            Console.WriteLine("Their product is " + (n * m));
+        }
+    }
+}
